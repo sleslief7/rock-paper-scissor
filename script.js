4bd3a5b919ff6455dadd64aed3fcddd7c6ce3dcd
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2024-04-30 10:19:43 -0400

    Changed array values to lowerCase

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let choices = ['Rock', 'Paper', 'Scissor'];
+let choices = ['rock', 'paper', 'scissor'];
 const getComputerChoice = () => {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
 }
