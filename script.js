a1aec345aa435904c7d0aa68744975a33043152a
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2024-04-30 10:25:16 -0400

    Created two global variables and returned humanChoice in getHumanChoice function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let choices = ['rock', 'paper', 'scissor'];
+let humanScore = 0;
+let computerScore = 0;
 
 const getComputerChoice = () => {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -6,4 +8,5 @@ const getComputerChoice = () => {
 
 const getHumanChoice = () => {
     let humanChoice = prompt('Choose rock, paper or scissor');
+    return humanChoice;
 }
