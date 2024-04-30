22c782869fb7dd3af940d36c37fc6b0d4379aaf5
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2024-04-30 10:28:22 -0400

    Fixed randomNumber variable to correspond choices array indexes

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let humanScore = 0;
 let computerScore = 0;
 
 const getComputerChoice = () => {
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    let randomNumber = Math.floor(Math.random() * 3);
+    let computerSelection = choices[randomNumber];
 }
 
 const getHumanChoice = () => {
