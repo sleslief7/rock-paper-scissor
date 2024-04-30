8c4c9fd35530776bd06880ced4ca713ca2487ae3
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2024-04-30 10:36:28 -0400

    Returned computerSelection in getComputerChoice
    Added trim() and toLowerCase() to the return value of getHumanChoice

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ let computerScore = 0;
 const getComputerChoice = () => {
     let randomNumber = Math.floor(Math.random() * 3);
     let computerSelection = choices[randomNumber];
+    return computerSelection;
 }
 
 const getHumanChoice = () => {
     let humanSelection = prompt('Choose rock, paper or scissor');
-    return humanSelection;
+    return humanSelection.trim().toLowerCase();
 }
