1b265597a0bea42bb61bf511784a2779640945a2
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2024-04-30 11:38:19 -0400

    Created is not valid function
    It allows me to check if the input from user is valid to run a while loop until it is valid

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ let choices = ['rock', 'paper', 'scissor'];
 let humanScore = 0;
 let computerScore = 0;
 
+const isNotValid = (str) => {
+    if(!str || 
+        str !== 'rock' &&
+        str !== 'paper' &&
+        str !== 'scissor'){
+            return true;
+        } else {
+            return false;
+        }
+}
+
 const getComputerChoice = () => {
     let randomNumber = Math.floor(Math.random() * 3);
     let computerSelection = choices[randomNumber];
@@ -9,6 +20,12 @@ const getComputerChoice = () => {
 }
 
 const getHumanChoice = () => {
-    let humanSelection = prompt('Choose rock, paper or scissor');
-    return humanSelection.trim().toLowerCase();
+    let humanSelection = prompt('Choose rock, paper or scissor').trim().toLowerCase();
+
+    while(isValid(humanSelection)) {
+        humanSelection = prompt('Please enter of the following valid answers: rock, paper or scissor').trim().toLowerCase();
+    }
+    if(humanSelection === 'rock' || humanSelection === 'paper' || humanSelection === 'scissor'){
+        return humanSelection;
+    }
 }
