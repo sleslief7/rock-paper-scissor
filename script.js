c570dfd22e6012ca5609539115cd44875ec307a1
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2024-04-30 10:22:18 -0400

    Declared geyHumanChoice function and humanChoice variable

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let choices = ['rock', 'paper', 'scissor'];
+
 const getComputerChoice = () => {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
+}
+
+const getHumanChoice = () => {
+    let humanChoice = prompt('Choose rock, papper or scissor');
 }
